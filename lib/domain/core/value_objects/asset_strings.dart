f8329a46d41eade2cd616444279c2c2e44a6e784
johnngugi <johnngugi777@gmail.com>
2021-08-24 15:45:53 +0300

    chore: remove unused assets

--- lib/domain/core/value_objects/asset_strings.dart
+++ lib/domain/core/value_objects/asset_strings.dart
@@ -1,16 +1,8 @@
-/// IMAGES
-const String careImg = 'assets/images/care.png';
-const String logoutImage = 'assets/images/logout.svg';
-
-/// Bottom navigation bar icon urls
-const String homeTabIconUrl = 'assets/icons/home.svg';
 const String patientsTabIconUrl = 'assets/icons/patients.svg';
-const String moreTabIconUrl = 'assets/icons/more.svg';
 const String helpTabIconUrl = 'assets/icons/help.svg';
 const String editIcon = 'assets/icons/edit.svg';
 
 // config file
-const String oneHealthPeopleIcon = 'assets/images/people.png';
 const String bewellAppBarLogo = 'assets/images/appbarlogo.png';
 
 const String patientExamZeroStateIconUrl = 'assets/images/add-records.png';
@@ -35,74 +27,35 @@ const String helpSearchIcon = 'assets/images/help_search.svg';
 const String userProfileIconUrl = 'assets/images/user_profile.svg';
 const String findPatientIconUrl = 'assets/images/find_patient.svg';
 const String addPatientIconUrl = 'assets/images/patient_add.svg';
-const String messageIconUrl = 'assets/images/message.svg';
 
 // icon urls
-const String searchPatientIcon = 'assets/images/search.svg';
-const String addPatientIcon = 'assets/images/add_patient.svg';
-const String peopleIconUrl = 'assets/images/people.svg';
-const String butterflyIconUrl = 'assets/images/butterfly.svg';
-const String homeIconUrl = 'assets/images/home_icon.svg';
-const String warningIconUrl = 'assets/icons/warning.png';
-const String infoIconUrl = 'assets/icons/info.png';
-const String successIconUrl = 'assets/icons/success.png';
-const String emptyFolderIconUrl = 'assets/images/emptyfolder.png';
-const String notificationBellIconUrl = 'assets/images/notification_bell.svg';
 const String doctorIcon = 'assets/images/doctor-2.jpg';
 
 const String landingPageImage = 'assets/images/landing.png';
 const String onboardingBackgroundImage = 'assets/images/onboarding_bg.png';
 
 /// ICONS
-const String getHelpIcon = 'assets/icons/support_icon.svg';
-const String chatIcon = 'assets/icons/chat.svg';
-const String callIcon = 'assets/icons/call.svg';
 const String warningImage = 'assets/icons/warning.png';
 const String errorImage = 'assets/icons/error.png';
 
 // config
-const String oneHealthIconUrl = 'assets/icons/one-health.png';
-const String frontDeskIconUrl = 'assets/icons/desk.png';
-const String patientsIconUrl = 'assets/icons/patients.png';
-const String waitingListIconUrl = 'assets/icons/waiting-list.png';
-const String authorizationIconUrl = 'assets/icons/authorizations.png';
-const String inboxIconUrl = 'assets/icons/inbox.png';
-const String rotaIconUrl = 'assets/icons/rota.png';
-const String setupIconUrl = 'assets/icons/setup.png';
-const String clinicalIconUrl = 'assets/icons/stethoscope.png';
-const String templateIconUrl = 'assets/icons/templates.png';
-const String labIconUrl = 'assets/icons/flask.png';
-const String medicineIconUrl = 'assets/icons/medicine.png';
 const String cameraIconUrl = 'assets/icons/camera.png';
-const String careIconUrl = 'assets/icons/care.png';
-const String moneyIconUrl = 'assets/icons/money.png';
 const String searchIconUrl = 'assets/icons/search.png';
 const String galleryIconUrl = 'assets/icons/gallery.png';
 
 const String checkIconUrl = 'assets/icons/check.png';
 
-const String scheduleIconUrl = 'assets/icons/schedule.svg';
-const String errorIconUrl = 'assets/icons/error.png';
 const String closeIconUrl = 'assets/icons/close.png';
-const String careBlueIconUrl = 'assets/icons/care_blue.png';
 const String bewellLogo = 'assets/icons/logo.svg';
 const String emptyIconUrl = 'assets/icons/empty_inbox.svg';
 const String emptyDataUrl = 'assets/images/no_data.svg';
 
-const String testingIconUrl1 = 'assets/icons/testing1.png';
-const String testingIconUrl2 = 'assets/icons/testing2.png';
-
 const String comingSoonUrl = 'assets/images/coming_soon.jpg';
-
-const String patientSearchIconUrl = 'assets/icons/patientsearch.png';
 
 const String errorDisplayImgUrl = 'assets/images/error-img.png';
 //Navigation Drawer Icons
-const String agentNavDrawerIconUrl = 'assets/icons/agent_nav_drawer_icon.svg';
-const String closeNavDrawerIconUrl = 'assets/icons/close_nav_drawer_icon.svg';
 const String helpNavDrawerIconUrl = 'assets/icons/help_nav_drawer_icon.svg';
 const String patientNavDrawerIconUrl =
     'assets/icons/patient_nav_drawer_icon.svg';
 
-const String testDevelopmentIconUrl = 'assets/images/test-development.png';
 const String favouriteHelpIconUrl = 'assets/images/favourite_action.png';
