b921c3928600ea5ca3d0dad5d6c8a25e260d23c5
Abiud Orina <abiudrn@gmail.com>
2021-09-15 16:36:43 +0300

    fix: switch tests docker image
    - run tests on CI on flutter v2.2.3
    
    Signed-off-by: Abiud Orina <abiudrn@gmail.com>

---
+++ install_flutter_ci.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -o pipefail
+echo "=== INSTALLING FLUTTER ==="
+git clone --single-branch --branch 2.2.3 -q https://github.com/flutter/flutter.git /home/circleci/flutter/
+export PATH=PATH="$PATH:/home/circleci/flutter/bin"
+flutter upgrade && flutter config --no-analytics
+flutter --version  
+flutter pub get
+echo "=== INSTALLING FIREBASE TOOLS ==="
+curl -sL https://firebase.tools | bash
